d057aac7c4eb16302dc7feb974dd7964559dc006
Dilantha-Wijesinghe <wdilantha66@gmail.com>
2023-04-02 10:41:12 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { Footer, Navbar } from "../components";
+import { Footer, Navbar } from '../components';
 import {
   About,
   Explore,
@@ -8,10 +8,10 @@ import {
   Insights,
   WhatsNew,
   World,
-} from "../sections";
+} from '../sections';
 
 const Home = () => (
-  <div id="body" >
+  <div id="body">
     <Navbar />
     <Hero />
     <div className="relative">
